ae9b7144f166f8ad922870673e39e41ab99afe57
ankith86 <55974015+ankith86@users.noreply.github.com>
2020-02-23 12:21:41 +0530

    changes

--- ColorList1/apk_builder3.sh
+++ ColorList1/apk_builder3.sh
@@ -5,7 +5,7 @@ DIR="$location/android/app/src/main/assets"
 
 function apk_Builder(){
 	 #cd $location/
-	 cd reactNative/ColorList
+	 cd ColorList
          echo " Working directory is $PWD "
 
          npx react-native bundle --dev false --platform android --entry-file index.js --bundle-output ./android/app/src/main/assets/index.android.bundle --assets-dest ./android/app/src/main/res
